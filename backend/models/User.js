15c1d673b848972aa02a54c892b6f19c3c1933b4
rokasj <rokas@jerio.me>
2023-06-12 16:08:32 -0400

    Create schema in User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const UserSchema = new Schema({
     username: {
         type: String,
         required: true
@@ -13,9 +13,89 @@ const userSchema = new Schema({
     hashedPassword: {
         type: String,
         required: true
-    }
+    },
+    goals: [
+        {
+            type: GoalSchema,
+            required: false
+        }
+    ],
+    routines: [
+        {
+            type: RoutineSchema,
+            required: false
+        }
+    ],
 }, {
     timestamps: true
 });
+
+const GoalSchema = new Schema({
+    description: {
+        type: String,
+        required: true
+    },
+    deadline: {
+        type: String,
+        required: false
+    },
+    exerciseEntries: [
+        {
+            type: ExerciseEntrySchema,
+            required: false
+        }
+    ]
+})
+
+const ExerciseEntrySchema = new Schema({
+    exercises: [
+        {
+            type: ExerciseSchema,
+            required: true
+        }
+    ],
+    date: {
+        type: String,
+        required: true
+    },
+    note: {
+        type: String,
+        required: false
+    }
+})
+
+const RoutineSchema = new Schema({
+    exercises: [
+        {
+            type: ExerciseSchema,
+            required: true
+        }
+    ]
+})
+
+const ExerciseSchema = new Schema({
+    sets: {
+        type: String,
+        required: function() {
+            return (!(this.reps || this.time) || !!(this.reps)) 
+        }
+    },
+    reps: {
+        type: String,
+        required: function () {
+            return (!(this.sets || this.time) || !!(this.sets))
+        }
+    },
+    time: {
+        type: String,
+        required: function () {
+            return !(this.reps || this.time)
+        }
+    },
+    name: {
+        type: String,
+        required: true
+    },
+})
 
 module.exports = mongoose.model('User', userSchema);

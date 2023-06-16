febecca6b3ff12ab3e8f4f3145ed494cfad2fb17
ohksam <SamOh37@gmail.com>
2023-06-15 20:19:09 -0400

    doctor, I think we've done it

--- backend/seeders/seeds.js
+++ backend/seeders/seeds.js
@@ -36,36 +36,40 @@ const seedData = async () => {
       updatedAt: Date.now(),
     });
 
-    // Create an exercise entry
-    const exerciseEntry = new ExerciseEntry({
-      date: '2023/07/05',
-      note: 'Completed workout',
-      rating: Math.floor(Math.random() * 5) + 1,
-    });
-
-    // Generate exercises and assign entryId and user references
-    const exercises = [];
-    for (let i = 0; i < 3; i++) {
-      const exercise = new Exercise({
-        name: 'Exercise ' + (i + 1),
-        sets: getRandomNumber(3, 5),
-        reps: getRandomNumber(8, 12),
-        setter: seedUser._id,
-        entryId: exerciseEntry._id,
+    // Generate exercise entries for 7 days
+    for (let day = 0; day < 7; day++) {
+      const exerciseEntry = new ExerciseEntry({
+        date: getNextDate(day),
+        note: 'Completed workout',
+        rating: Math.floor(Math.random() * 5) + 1,
       });
-      exercises.push(exercise);
-      seedUser.exercises.push(exercise);
-      exerciseEntry.exercises.push(exercise);
-    }
 
-    // Assign exercise entry and exercises to the goal
-    seedGoal.exerciseEntries.push(exerciseEntry);
+      // Generate exercises and assign entryId and user references
+      const exercises = [];
+      for (let i = 0; i < 3; i++) {
+        const exercise = new Exercise({
+          name: 'Exercise ' + (i + 1),
+          sets: getRandomNumber(3, 5),
+          reps: getRandomNumber(8, 12),
+          setter: seedUser._id,
+          entryId: exerciseEntry._id,
+        });
+        exercises.push(exercise);
+        seedUser.exercises.push(exercise);
+        exerciseEntry.exercises.push(exercise);
+      }
+
+      // Assign exercise entry and exercises to the goal
+      seedGoal.exerciseEntries.push(exerciseEntry);
+
+      // Save the exercises to the database
+      await Promise.all(exercises.map(exercise => exercise.save()));
+    }
 
     // Assign goal to the user
     seedUser.goals.push(seedGoal);
 
-    // Save the user and exercises to the database
-    await Promise.all(exercises.map(exercise => exercise.save()));
+    // Save the user to the database
     await seedUser.save();
 
     console.log('Seed data created successfully!');
@@ -80,6 +84,16 @@ const seedData = async () => {
 // Generate a random number within a range
 const getRandomNumber = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+// Get the next date in the week
+const getNextDate = (day) => {
+  const today = new Date();
+  today.setDate(today.getDate() + day);
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, '0');
+  const date = String(today.getDate()).padStart(2, '0');
+  return `${year}/${month}/${date}`;
 };
 
 // Call the function to create the seed data

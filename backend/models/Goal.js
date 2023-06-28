2098adc094e2f0560fef53096efa1cd66f1908f2
ohksam <SamOh37@gmail.com>
2023-06-27 22:08:48 -0400

    refactor goals router, multiple schemas

--- backend/models/Goal.js
+++ backend/models/Goal.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const ExerciseEntrySchema = require('./ExerciseEntry');
 
 const GoalSchema = new Schema({
     title: {
@@ -19,17 +18,13 @@ const GoalSchema = new Schema({
         type: String,
         required: false
     },
-    exerciseEntries: [
-        {
-            type: ExerciseEntrySchema,
-            required: false
-        }
-    ],
-    updatedAt: {
-        type: Date,
-        default: Date.now
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
     }
-})
+}, {
+    timestamps: true
+});
 
 module.exports = GoalSchema;
 mongoose.model('Goal', GoalSchema);

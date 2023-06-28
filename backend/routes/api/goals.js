1a6227899e8e231e25b8a7d4b11019cb62ebae29
ohksam <SamOh37@gmail.com>
2023-06-28 18:05:25 -0400

    wrote more goal routes

--- backend/routes/api/goals.js
+++ backend/routes/api/goals.js
@@ -18,6 +18,10 @@ router.post('/', requireUser, validateGoalInput, async (req, res, next) => {
         });
 
         let goal = await newGoal.save();
+
+        req.user.currentGoal = goal;
+        await req.user.save();
+
         goal = await goal.populate('user', '_id username imgUrl createdAt');
         return res.json(goal);
     }
@@ -25,5 +29,88 @@ router.post('/', requireUser, validateGoalInput, async (req, res, next) => {
         next(err);
     }
 });
+
+// view a specific goal (GET)
+router.get('/:goalId', requireUser, async (req, res, next) => {
+    try {
+        const goal = await Goal.findById(req.params.goalId)
+            .populate('user', '_id username imgUrl createdAt');
+        if (!goal) {
+            const error = new Error('Goal not found');
+            error.statusCode = 404;
+            throw error;
+        }
+        return res.json(goal);
+    } catch (err) {
+        next(err);
+    }
+});
+
+// edit a goal (patch)
+router.patch('/:goalId', requireUser, validateGoalInput, async (req, res, next) => {
+    try {
+        const goal = await Goal.findById(req.params.goalId);
+        if (!goal) {
+            const error = new Error('Goal not found');
+            error.statusCode = 404;
+            throw error;
+        } else if (req.user !== goal.user) {
+            const error = new Error('Cannot edit this goal');
+            error.statusCode = 403;
+            throw error;
+        }
+
+        goal.title = req.body.title || goal.title
+        goal.description = req.body.description || goal.description;
+        goal.deadline = req.body.deadline || goal.deadline;
+        goal.completionDate = req.body.completionDate || goal.completionDate;
+
+        await goal.save();
+
+        req.user.currentGoal = goal;
+        await req.user.save();
+
+        goal = await goal.populate('user', '_id username imgUrl createdAt');
+
+        return res.json(goal);
+    } catch (err) {
+        next(err);
+    }
+});
+
+// delete a goal (delete)
+router.delete('/:goalId', requireUser, async (req, res, next) => {
+    try {
+        const goal = await Goal.findOneAndDelete({ _id: req.params.goalId });
+        if (!goal) {
+            const error = new Error('Goal not found');
+            error.statusCode = 404;
+            throw error;
+        }
+
+        if (req.user.currentGoal && req.user.currentGoal._id === goal._id) {
+            req.user.currentGoal = null;
+            await req.user.save();
+        }
+
+        return res.json({ message: 'Goal successfully deleted' });
+    } catch (err) {
+        next(err);
+    }
+});
+
+// fetch all goals per user
+router.get('/all/:userId', requireUser, async (req, res, next) => {
+    try {
+        const goals = await Goal.find({ user: req.params.userId })
+            .populate('user', '_id username imgUrl createdAt');
+
+        return res.json(goals);
+    } catch (err) {
+        next(err);
+    }
+});
+
+
 
 module.exports = router;

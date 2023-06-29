b4cefd24aa0f305b34c0733827ee310dfa3342fb
rokasj <rokas@jerio.me>
2023-06-28 20:02:19 -0400

    Update passport loginUser with currentGoal

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -27,7 +27,8 @@ exports.loginUser = async function (user) {
     const userInfo = {
         _id: user._id,
         username: user.username,
-        email: user.email
+        email: user.email,
+        currentGoal: user.currentGoal
     };
     const token = await jwt.sign(
         userInfo, // payload

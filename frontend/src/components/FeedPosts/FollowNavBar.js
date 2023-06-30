bbe46d7b20ad87a73c5815e22a46c01e9ae79f7d
carveyh <carvey.hor@gmail.com>
2023-06-30 13:14:21 -0400

    Organize site & feed navbar

--- frontend/src/components/FeedPosts/FollowNavBar.js
+++ frontend/src/components/FeedPosts/FollowNavBar.js
@@ -1,14 +1,15 @@
 import "./FollowNavBar.css";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const FollowNavBar = () => {
 	const sessionUser = useSelector(state => state.session.user);
 	return (
 		<div className="follow-nav-bar-container">
 			<ul>
-				<li>Follows</li>
-				<li>Discover</li>
-				<li>{sessionUser.username}</li>
+				<li><Link to={`/feed`}>Follows</Link></li>
+				<li><Link to={`/feed`}>Discover</Link></li>
+				<li><Link to={`/feed/${sessionUser._id}`}>{sessionUser.username}</Link></li>
 			</ul>
 		</div>
 	)

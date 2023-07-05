9b24885442646a1aacf7095c01e06897aca728ad
Michele Zhang <michelezhang98@gmail.com>
2023-07-05 10:51:35 -0400

    updates some spacing

--- frontend/src/components/LandingPage/LandingGifContainer.js
+++ frontend/src/components/LandingPage/LandingGifContainer.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 
 const LandingGifContainer = () => {
 	// let imgName = "narutoirl";
-//   const imgNames = ["narutoirl", "swim", "warmup", "narutoirl", "dekupushup", "bicyclecrunch", "jogging", "narutoirl", "tyingshoes", "yoga"]
+	// const imgNames = ["narutoirl", "swim", "warmup", "narutoirl", "dekupushup", "bicyclecrunch", "jogging", "narutoirl", "tyingshoes", "yoga"]
 	const imgNames = ["narutoirl", "swim", "warmup", "narutoirl", "dekupushup", "bicyclecrunch", "jogging", "narutoirl", "tyingshoes"]
 
 

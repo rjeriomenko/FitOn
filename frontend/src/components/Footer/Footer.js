88dc77efe9f38e3f26d252d0aac73c8b5d501d66
carveyh <carvey.hor@gmail.com>
2023-07-10 13:15:58 -0400

    Update team links and opens in new tab

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -13,24 +13,24 @@ const Footer = () => {
 			{/* <div className={`${landing ? `team-links-light` : `team-links-dark`} site-footer-container`}> */}
 			<div className={` site-footer-container`}>
 				<div className={`team-links rokas-links`}>
-					<a href={`https://github.com/rjeriomenko`}><span className='team-links-text'>Rokas</span></a>
-					<a href={`https://github.com/rjeriomenko`}><span><i className="fa-brands fa-github"></i></span></a>
-					<a href={`https://www.linkedin.com/in/rokas-jeriomenko-82b312121/`}><span><i className="fa-brands fa-linkedin"></i></span></a>
+					<a target="_blank" href={`https://github.com/rjeriomenko`}><span className='team-links-text'>Rokas</span></a>
+					<a target="_blank" href={`https://github.com/rjeriomenko`}><span><i className="fa-brands fa-github"></i></span></a>
+					<a target="_blank" href={`https://www.linkedin.com/in/rokas-jeriomenko-82b312121/`}><span><i className="fa-brands fa-linkedin"></i></span></a>
 				</div>
 				<div className='team-links michele-links'>
-					<a href={`https://github.com/mzhanggg`}><span className='team-links-text'>Michele</span></a>
-					<a href={`https://github.com/mzhanggg`}><span><i className="fa-brands fa-github"></i></span></a>
-					<a href={`https://www.linkedin.com/in/michele-zhang-380417199/`}><span><i className="fa-brands fa-linkedin"></i></span></a>
+					<a target="_blank" href={`https://github.com/mzhanggg`}><span className='team-links-text'>Michele</span></a>
+					<a target="_blank" href={`https://github.com/mzhanggg`}><span><i className="fa-brands fa-github"></i></span></a>
+					<a target="_blank" href={`https://www.linkedin.com/in/michele-zhang-380417199/`}><span><i className="fa-brands fa-linkedin"></i></span></a>
 				</div>
 				<div className='team-links sam-links'>
-					<a href={`https://github.com/ohksam`}><span className='team-links-text'>Sam</span></a>
-					<a href={`https://github.com/ohksam`}><span><i className="fa-brands fa-github"></i></span></a>
-					<a href={`https://github.com/ohksam`}><span><i className="fa-brands fa-linkedin"></i></span></a>
+					<a target="_blank" href={`https://github.com/ohksam`}><span className='team-links-text'>Sam</span></a>
+					<a target="_blank" href={`https://github.com/ohksam`}><span><i className="fa-brands fa-github"></i></span></a>
+					<a target="_blank" href={`https://www.linkedin.com/in/sam-oh-0abb8327b/`}><span><i className="fa-brands fa-linkedin"></i></span></a>
 				</div>
 				<div className='team-links carvey-links'>
-					<a href={`https://github.com/carveyh`}><span className='team-links-text'>Carvey</span></a>
-					<a href={`https://github.com/carveyh`}><span><i className="fa-brands fa-github"></i></span></a>
-					<a href={`https://www.linkedin.com/in/carvey-hor/`}><span><i className="fa-brands fa-linkedin"></i></span></a>
+					<a target="_blank" href={`https://github.com/carveyh`}><span className='team-links-text'>Carvey</span></a>
+					<a target="_blank" href={`https://github.com/carveyh`}><span><i className="fa-brands fa-github"></i></span></a>
+					<a target="_blank" href={`https://www.linkedin.com/in/carvey-hor/`}><span><i className="fa-brands fa-linkedin"></i></span></a>
 				</div>
 			</div>
 			<div className='copyright'>Copyright &copy; 2023 goalgetters</div>

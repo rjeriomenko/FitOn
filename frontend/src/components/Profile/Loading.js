4ce837a28df62aed52d080e759eaf9767c793c45
Michele Zhang <michelezhang98@gmail.com>
2023-07-10 11:52:13 -0400

    add loading to dataVis

---
+++ frontend/src/components/Profile/Loading.js
@@ -0,0 +1,13 @@
+function Loading () {
+
+
+    return (
+        <>
+            <div>
+                <img className="loading" src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif"></img>
+            </div>
+        </>
+    )
+}
+
+export default Loading;
